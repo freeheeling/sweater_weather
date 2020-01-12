ffecad7f9dd58cf46669f6aa7a5ddac3e91d098e
Scott Regenthal <scottregenthal@gmail.com>
2020-01-12 14:05:51 -0700

    Refactor controller and create service object

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,17 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
     location = params[:location]
-
-    # unsplash
-    conn = Faraday.new(url: 'https://api.unsplash.com/') do |f|
-      f.adapter(Faraday.default_adapter)
-      f.params['client_id'] = ENV['UNSPLASH_KEY']
-      f.params['query'] = location
-    end
-
-    response = conn.get('search/photos')
-    images_data = JSON.parse(response.body, symbolize_names: true)
-
-    image_uri = images_data[:results][0][:urls][:raw]
+    background_image = BackgroundFacade.new(location)
+    render json: BackgroundSerializer.new(background_image)
   end
 end
