bf684001cc52fc5ad4c024e07aeb50717a9cdb67
Scott Regenthal <scottregenthal@gmail.com>
2020-01-11 21:38:35 -0700

    Add forecast api request test

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Forecast API' do
+  it 'returns weather data for a given location' do
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+  end
+end
