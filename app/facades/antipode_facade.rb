400e4270bc03c59e409882819bbd05651db6a311
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 11:24:01 -0700

    Test refactor

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -2,8 +2,24 @@ class AntipodeFacade
   attr_reader :id
 
   def initialize(location)
-    @id = nil
+    @id = 1
     @location = location
+  end
+
+  def type
+    'antipode'
+  end
+
+  def lat
+    GeocodeService.new.coords(location)[:lat]
+  end
+
+  def long
+    GeocodeService.new.coords(location)[:lng]
+  end
+
+  def location_name
+    ReverseGeocodeService.new.city(location)
   end
 
   def anti_lat
@@ -12,6 +28,18 @@ class AntipodeFacade
 
   def anti_long
     AmypodeService.new.anti_coords(location)[:long]
+  end
+
+  def current_temperature
+    DarkskyService.new.current(anti_lat, anti_long)[:temperature]
+  end
+
+  def summary
+    DarkskyService.new.daily(anti_lat, anti_long)[:summary]
+  end
+
+  def search_location
+    location
   end
 
   private
--- app/services/reverse_geocode_service.rb
+++ app/services/reverse_geocode_service.rb
@@ -1,16 +1,16 @@
 class ReverseGeocodeService
-  def city(location)
-    get_json_city(location, 'geocode/json')
+  def city(anti_lat, anti_long)
+    get_json_city(anti_lat, anti_long, 'geocode/json')
   end
 
   private
 
-    def get_json_city(location, url)
-      response = conn(location).get(url)
+    def get_json_city(anti_lat, anti_long, url)
+      response = conn(anti_lat, anti_long).get(url)
       JSON.parse(response.body, symbolize_names: true)[:results][0][:address_components][0][:long_name]
     end
 
-    def conn(location)
+    def conn(anti_lat, anti_long)
       Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
         f.adapter(Faraday.default_adapter)
         f.params['key'] = ENV['GEOCODE_KEY']
