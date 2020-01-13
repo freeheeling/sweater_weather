dce8c44dd4db19aec3b3f0d342b56619cb727147
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 09:57:38 -0700

    Get connection to amypode

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,7 +1,33 @@
-class AntipodeController < ApplicationController
+class Api::V1::AntipodeController < ApplicationController
   def index
     location = params[:location]
-    antipode_data = AntipodeFacade.new(location)
-    render json: AntipodeSerializer.new(antipode_data)
+
+    # geocode
+    conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
+      f.adapter(Faraday.default_adapter)
+      f.params['key'] = ENV['GEOCODE_KEY']
+      f.params['address'] = location
+    end
+
+    response = conn.get('geocode/json')
+    location_data = JSON.parse(response.body, symbolize_names: true)
+
+    lat = location_data[:results][0][:geometry][:location][:lat]
+    long = location_data[:results][0][:geometry][:location][:lng]
+
+    # amypode
+    conn = Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
+      f.adapter(Faraday.default_adapter)
+      f.headers['api_key'] = ENV['ANTIPODE_KEY']
+      f.params['lat'] = lat
+      f.params['long'] = long
+    end
+
+    response = conn.get("antipode?lat=#{lat}&long=#{long}")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    # antipode_data = AntipodeFacade.new(location)
+    # render json: AntipodeSerializer.new(antipode_data)
   end
 end
--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -1,12 +1,20 @@
 class AnitpodeService
+  def
+    get_json
+  end
 
   private
 
     def get_json
-
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
     end
 
     def conn(location)
-
+      Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
+        f.adapter(Faraday.default_adapter)
+        f.headers['api_key'] = ENV['ANTIPODE_KEY']
+        f.params['lat'] = lat
+        f.params['long'] = long
     end
 end
