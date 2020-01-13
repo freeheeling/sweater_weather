60282964147ab2dc77411099e422f4699dce17dc
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 11:47:48 -0700

    Update tests with stubbed requests and add fixtures

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,54 +1,54 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
     location = params[:location]
-    # 
-    # # geocode
-    # conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
-    #   f.adapter(Faraday.default_adapter)
-    #   f.params['key'] = ENV['GEOCODE_KEY']
-    #   f.params['address'] = location
-    # end
-    #
-    # response = conn.get('geocode/json')
-    # location_data = JSON.parse(response.body, symbolize_names: true)
-    #
-    # lat = location_data[:results][0][:geometry][:location][:lat]
-    # long = location_data[:results][0][:geometry][:location][:lng]
-    #
-    # # amypode
-    # amypode_conn = Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
-    #   f.adapter(Faraday.default_adapter)
-    #   f.headers['api_key'] = ENV['AMYPODE_KEY']
-    # end
-    #
-    # amypode_response = amypode_conn.get("antipodes?lat=#{lat}&long=#{long}")
-    # antipode_data = JSON.parse(amypode_response.body, symbolize_names: true)
-    #
-    # anti_lat = antipode_data[:data][:attributes][:lat]
-    # anti_long = antipode_data[:data][:attributes][:long]
-    #
-    # # reverse geocode
-    # reverse_conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
-    #   f.adapter(Faraday.default_adapter)
-    #   f.params['key'] = ENV['GEOCODE_KEY']
-    #   f.params['latlng'] = "#{anti_lat},#{anti_long}"
-    # end
-    #
-    # reverse_response = reverse_conn.get('geocode/json')
-    # reverse_location_data = JSON.parse(reverse_response.body, symbolize_names: true)
-    #
-    # antipode_city = reverse_location_data[:results][0][:address_components][0][:long_name]
-    #
-    # # darksky
-    # darksky_conn = Faraday.new(url: 'https://api.darksky.net') do |f|
-    #   f.adapter(Faraday.default_adapter)
-    # end
-    #
-    # darsky_response = darksky_conn.get("/forecast/#{ENV['DARKSKY_KEY']}/#{anti_lat},#{anti_long}")
-    # weather_data = JSON.parse(darsky_response.body, symbolize_names: true)
-    #
-    # current_temp = weather_data[:currently][:temperature]
-    # summary = weather_data[:daily][:data][0][:summary]
+
+    # geocode
+    conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
+      f.adapter(Faraday.default_adapter)
+      f.params['key'] = ENV['GEOCODE_KEY']
+      f.params['address'] = location
+    end
+
+    response = conn.get('geocode/json')
+    location_data = JSON.parse(response.body, symbolize_names: true)
+
+    lat = location_data[:results][0][:geometry][:location][:lat]
+    long = location_data[:results][0][:geometry][:location][:lng]
+
+    # amypode
+    amypode_conn = Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
+      f.adapter(Faraday.default_adapter)
+      f.headers['api_key'] = ENV['AMYPODE_KEY']
+    end
+
+    amypode_response = amypode_conn.get("antipodes?lat=#{lat}&long=#{long}")
+    antipode_data = JSON.parse(amypode_response.body, symbolize_names: true)
+
+    anti_lat = antipode_data[:data][:attributes][:lat]
+    anti_long = antipode_data[:data][:attributes][:long]
+
+    # reverse geocode
+    reverse_conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
+      f.adapter(Faraday.default_adapter)
+      f.params['key'] = ENV['GEOCODE_KEY']
+      f.params['latlng'] = "#{anti_lat},#{anti_long}"
+    end
+
+    reverse_response = reverse_conn.get('geocode/json')
+    reverse_location_data = JSON.parse(reverse_response.body, symbolize_names: true)
+
+    antipode_city = reverse_location_data[:results][0][:address_components][0][:long_name]
+
+    # darksky
+    darksky_conn = Faraday.new(url: 'https://api.darksky.net') do |f|
+      f.adapter(Faraday.default_adapter)
+    end
+
+    darsky_response = darksky_conn.get("/forecast/#{ENV['DARKSKY_KEY']}/#{anti_lat},#{anti_long}")
+    weather_data = JSON.parse(darsky_response.body, symbolize_names: true)
+
+    current_temp = weather_data[:currently][:temperature]
+    summary = weather_data[:daily][:data][0][:summary]
 
     antipode_data = AntipodeFacade.new(location)
     render json: AntipodeSerializer.new(antipode_data)
--- spec/requests/api/v1/antipode_request_spec.rb
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 describe 'Antipode' do
   it 'returns the antipode city and forecast' do
     city = 'hong kong'
+    geocode_uri = "https://maps.googleapis.com/maps/api/geocode/json?address=#{city}&key=#{ENV['GEOCODE_KEY']}"
+    amypode_uri = 'http://amypode.herokuapp.com/api/v1/antipodes?lat=22.3193039&long=114.1693611'
+    reverse_geocode_uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=-22.3193039,-65.8306389&key=#{ENV['GEOCODE_KEY']}"
+    darksky_uri = "https://api.darksky.net/forecast/#{ENV['DARKSKY_KEY']}/-22.3193039,-65.8306389"
+
+    geocode_json_response = File.read('spec/fixtures/geocode_location_data.json')
+    amypode_json_response = File.read('spec/fixtures/amypode_location_data.json')
+    reverse_geocode_json_response = File.read('spec/fixtures/reverse_geocode_location_data.json')
+    darksky_json_response = File.read('spec/fixtures/darksky_forecast_data.json')
+
+    stub_request(:get, geocode_uri).to_return(status: 200, body: geocode_json_response)
+    stub_request(:get, amypode_uri).to_return(status: 200, body: amypode_json_response)
+    stub_request(:get, reverse_geocode_uri).to_return(status: 200, body: reverse_geocode_json_response)
+    stub_request(:get, darksky_uri).to_return(status: 200, body: darksky_json_response)
 
     get "/api/v1/antipode?location=#{city}"
 
