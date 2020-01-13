020a08d1d58e6f91bae3e2364515127dea48a959
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 10:38:49 -0700

    Connect to darksky

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -16,30 +16,40 @@ class Api::V1::AntipodeController < ApplicationController
     long = location_data[:results][0][:geometry][:location][:lng]
 
     # amypode
-    conn = Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
+    amypode_conn = Faraday.new(url: 'http://amypode.herokuapp.com/api/v1/') do |f|
       f.adapter(Faraday.default_adapter)
       f.headers['api_key'] = ENV['AMYPODE_KEY']
     end
 
-    response = conn.get("antipodes?lat=#{lat}&long=#{long}")
+    amypode_response = amypode_conn.get("antipodes?lat=#{lat}&long=#{long}")
 
-    antipode_data = JSON.parse(response.body, symbolize_names: true)
+    antipode_data = JSON.parse(amypode_response.body, symbolize_names: true)
 
     anti_lat = antipode_data[:data][:attributes][:lat]
     anti_long = antipode_data[:data][:attributes][:long]
 
     # geocode
-    conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
+    reverse_conn = Faraday.new(url: 'https://maps.googleapis.com/maps/api/') do |f|
       f.adapter(Faraday.default_adapter)
       f.params['key'] = ENV['GEOCODE_KEY']
       f.params['latlng'] = "#{anti_lat},#{anti_long}"
     end
 
-    response = conn.get('geocode/json')
-    location_data = JSON.parse(response.body, symbolize_names: true)
+    reverse_response = reverse_conn.get('geocode/json')
+    reverse_location_data = JSON.parse(reverse_response.body, symbolize_names: true)
+
+    # darksky
+    darksky_conn = Faraday.new(url: 'https://api.darksky.net') do |f|
+      f.adapter(Faraday.default_adapter)
+    end
+
+    darsky_response = darksky_conn.get("/forecast/#{ENV['DARKSKY_KEY']}/#{anti_lat},#{anti_long}")
+    weather_data = JSON.parse(darsky_response.body, symbolize_names: true)
+
+    current_temp = weather_data[:currently][:temperature]
+    summary = weather_data[:daily][:data][0][:summary]
 
     require "pry"; binding.pry
-
     # antipode_data = AntipodeFacade.new(location)
     # render json: AntipodeSerializer.new(antipode_data)
   end
