c4d58fdd0c91cee6854163b0305eb955d51a59e1
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 17:58:40 -0700

    Change testing from webmock to vcr

--- spec/requests/api/v1/background_image_request_spec.rb
+++ spec/requests/api/v1/background_image_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'Image API' do
-  it 'returns a background image for a given location' do
-    unsplash_uri = ("https://api.unsplash.com/search/photos?client_id=#{ENV['UNSPLASH_KEY']}&query=denver,co")
-    unsplash_json_response = File.read('spec/fixtures/unsplash_image_data.json')
-
-    stub_request(:get, unsplash_uri).to_return(status: 200, body: unsplash_json_response)
+  it 'returns a background image for a given location', :vcr do
+    # unsplash_uri = ("https://api.unsplash.com/search/photos?client_id=#{ENV['UNSPLASH_KEY']}&query=denver,co")
+    # unsplash_json_response = File.read('spec/fixtures/unsplash_image_data.json')
+    #
+    # stub_request(:get, unsplash_uri).to_return(status: 200, body: unsplash_json_response)
 
     get '/api/v1/backgrounds?location=denver,co'
 
