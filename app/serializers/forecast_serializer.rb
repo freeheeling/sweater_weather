07afc123a4aa885e142ff098763d43881d043c94
Scott Regenthal <scottregenthal@gmail.com>
2020-01-13 21:53:35 -0700

    Refactor forecast facade and serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -2,27 +2,8 @@ class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
   attributes :id,
-             :lat,
-             :long,
-             :city,
-             :state,
-             :country,
-             :current_date,
-             :current_temp,
-             :feels_like,
-             :current_icon,
-             :humidity,
-             :visibility,
-             :uv_index,
-             :summary_today,
-             :summary_tonight,
-             :daily_day,
-             :daily_icon,
-             :daily_high,
-             :daily_low,
-             :daily_precip_percent,
-             :daily_precip_type,
-             :hourly_icon,
-             :hourly_time,
-             :hourly_temp
+             :search_location,
+             :current_forecast,
+             :daily_forecast,
+             :hourly_forecast
 end
