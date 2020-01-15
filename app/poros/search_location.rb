9fdc026b7449b2f07b0623632e49fb6dfaeb28a1
Scott Regenthal <scottregenthal@gmail.com>
2020-01-15 13:56:57 -0700

    Remove enumerable, create private method

--- app/poros/search_location.rb
+++ app/poros/search_location.rb
@@ -4,12 +4,14 @@ class SearchLocation
   end
 
   def city_state_country
-    @location_data.map do |data|
-      {
-        city: data[0][:long_name],
-        state: data[2][:short_name],
-        country: data[3][:long_name]
-      }
-    end
+    {
+      city: location_data[0][:long_name],
+      state_abbrev: location_data[2][:short_name],
+      country: location_data[3][:long_name]
+    }
   end
+
+  private
+
+    attr_reader :location_data
 end
