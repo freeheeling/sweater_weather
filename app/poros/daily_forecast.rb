b4b34575a65bd5671c0bb37529338b2c7791c8b2
Scott Regenthal <scottregenthal@gmail.com>
2020-01-15 13:57:50 -0700

    Modify attribute descriptions

--- app/poros/daily_forecast.rb
+++ app/poros/daily_forecast.rb
@@ -6,13 +6,13 @@ class DailyForecast
   def next_five_days
     @daily_data.map do |day|
       {
-        summary_today: day[:summary],
-        daily_day: Time.at(day[:time]).strftime('%A'),
-        daily_high: (day[:temperatureHigh]).round,
-        daily_low: (day[:temperatureLow]).round,
-        daily_icon: day[:icon],
-        daily_precip_percent: (day[:precipProbability]*100).to_i,
-        daily_precip_type: day[:precipType]
+        summary: day[:summary],
+        day: Time.at(day[:time]).strftime('%A'),
+        high_temp_F: (day[:temperatureHigh]).round,
+        low_temp_F: (day[:temperatureLow]).round,
+        icon_description: day[:icon],
+        precip_percent: (day[:precipProbability]*100).to_i,
+        precip_type: day[:precipType]
       }
     end[1..5]
   end
